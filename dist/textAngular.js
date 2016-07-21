66d86d30b413769a2d786f376280f6aa08e7aa39
MooYeol Prescott Lee <mooyoul@gmail.com>
2016-07-21 17:16:38 +0900

    chore(release): Build Dist files

--- dist/textAngular.js
+++ dist/textAngular.js
@@ -2,7 +2,7 @@
 @license textAngular
 Author : Austin Anderson
 License : 2013 MIT
-Version 1.4.12
+Version 1.4.13
 
 See README.md or https://github.com/fraywing/textAngular/wiki for requirements and use.
 */
